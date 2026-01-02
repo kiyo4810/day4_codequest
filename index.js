03e049787d2c0a3cf1ac7cc9f00f604e4393cdf7
kiyo4810 <kiyo4810@gmail.com>
2026-01-02 11:26:46 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+/* https://codequest.work/javascript-practice-problems-beginner/
+を使っての練習
+*/
+// Q1
+'use strict';
+const message = 'Hello, JavaScript!';
+console.log(message);
+document.getElementById("message").innerText = message;
+// Q2
+let suuji1 = 3;
+let suuji2 = 4;
+console.log(suuji1 + suuji2);
+document.getElementById("plus").innerText = suuji1 + suuji2;
+console.log(suuji1 - suuji2);
+document.getElementById("minus").innerText = suuji1 - suuji2;
+console.log(suuji1 * suuji2);
+document.getElementById("multiple").innerText = suuji1 * suuji2;
+console.log(suuji1 / suuji2);
+document.getElementById("divide").innerText = suuji1 / suuji2;
+console.log(suuji1 % suuji2);
+document.getElementById("amari").innerText = suuji1 % suuji2;
+console.log(suuji1 & suuji2);
+document.getElementById("ando").innerText = suuji1 & suuji2;
+
