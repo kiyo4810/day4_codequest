b5e3f093ebab1d0fd1d7215e14633a15dd3032a9
kiyo4810 <kiyo4810@gmail.com>
2026-01-02 13:37:43 +0900

    Q4 if続き修正

--- index.js
+++ index.js
@@ -43,9 +43,9 @@ let yourScore = 81;
 document.getElementById("yrscr").innerText = yourScore;
 
 if (yourScore >=80) {
-    console.log ("合格");
+    console.log("合格");
     document.getElementById("okornot").innerText = "合格"
 } else {
-    console log ("不合格");
+    console log("不合格");
     document.getElementById("okornot").innerText = "不合格"
 }
